53b58630839de3f2c02c9c23209c30281871b90e
Thinh Vo <75603309+superman482@users.noreply.github.com>
2022-11-10 10:59:22 -0800

    Changes_Auto_Grader

--- TestListExamples.java
+++ TestListExamples.java
@@ -3,4 +3,38 @@ import org.junit.*;
 
 public class TestListExamples {
   // Write your grading tests here!
+
+  @Test
+  public void testFilter() {
+    StringChecker sc = new Checker();
+    List<String> input1 = new ArrayList<>();
+    input1.add("hello");
+    input1.add("world");
+    input1.add("hi");
+    List<String> expected = new ArrayList<>();
+    expected.add("hello");
+    expected.add("hi");
+    for(int i = 0; i < expected.size(); i++) {
+        assertEquals(expected.get(i), ListExamples.filter(input1, sc).get(i));
+    }
+    // assertEquals(expected, ListExamples.filter(input1, sc));
+  }
+
+  @Test
+    public void testMerge() {
+        List<String> input1 = new ArrayList<>();
+        input1.add("g");
+        input1.add("e");
+        List<String> input2 = new ArrayList<>();
+        input2.add("r");
+        input2.add("b");
+        List<String> expected = new ArrayList<>();
+        expected.add("b");
+        expected.add("e");
+        expected.add("g");
+        expected.add("r");
+        for(int i = 0; i < expected.size(); i++) {
+            assertEquals(expected.get(i), ListExamples.merge(input1, input2).get(i));
+        }
+    }
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,27 @@
 # Create your grading script here
+CP = ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar"
+CP1 = ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar"
 
 set -e
 
 rm -rf student-submission
+
+echo "Student-submission is deleted"
+
 git clone $1 student-submission
 
+echo "Finished cloning student-submission"
+
+cp TestListExamples.java student-submission/
+
+echo "Copied Test Successfully"
+
+cd student-submission/
+
+echo "In student-submission/"
+
+javac -cp $CP *.java
+
+echo "Compiled"
+
+java -cp $CP1 org.junit.runner.JUnitCore TestListExamples
